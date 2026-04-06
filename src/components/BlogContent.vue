62351f3e8ff9ca794aec9f6d7cf0d1834d969705
Hirotaka Sato <laggyboy@outlook.jp>
2026-04-07 08:05:38 +0900

    blogコンポーネントの作成開始

---
+++ src/components/BlogContent.vue
@@ -0,0 +1,13 @@
+
+<script setup>
+</script>
+
+<template>
+  <div id="career" class="m-1">
+    <article>
+      <p>testcontent</p>
+    </article>
+  </div>
+</template>
+
+<style></style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,8 +1,11 @@
-<script setup></script>
+<script setup>
+  import Blog from '@/components/BlogContent.vue'
+</script>
 
 <template>
   <div class="container mx-auto m-5 min-w-xs">
-    <p>testpage</p>
+    <h3 class="text-2xl">Blog</h3>
+    <Blog />
   </div>
 </template>
 
