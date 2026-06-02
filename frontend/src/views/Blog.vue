c75e4fc42c257c038aa678ba634a981fb7d3f026
Hirotaka Sato <laggyboy@outlook.jp>
2026-06-02 22:23:26 +0900

    エラー処理追加

--- frontend/src/views/Blog.vue
+++ frontend/src/views/Blog.vue
@@ -7,6 +7,7 @@ const blogs = ref([]);
 const fetchBlogs = async () => {
   try {
     const res = await fetch(`${import.meta.env.VITE_API_URL}/blogs`);
+    if (!res.ok) throw new Error(`API error: ${res.status}`);
     blogs.value = await res.json();
   } catch (e) {
     console.error('Fail to fetch blogs', e);
