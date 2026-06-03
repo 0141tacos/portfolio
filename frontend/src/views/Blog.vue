a144bb33f458608dde2d7937df220c4fbfd0ab50
Hirotaka Sato <laggyboy@outlook.jp>
2026-06-04 07:10:55 +0900

    処理中、エラー時の挙動追加

--- frontend/src/views/Blog.vue
+++ frontend/src/views/Blog.vue
@@ -3,14 +3,20 @@ import { ref } from 'vue';
 import BlogContent from '@/components/BlogContent.vue';
 
 const blogs = ref([]);
+const blogLoading = ref(false);
+const blogError = ref(null);
 
 const fetchBlogs = async () => {
   try {
+    blogLoading.value = true;
     const res = await fetch(`${import.meta.env.VITE_API_URL}/blogs`);
     if (!res.ok) throw new Error(`API error: ${res.status}`);
     blogs.value = await res.json();
   } catch (e) {
-    console.error('Fail to fetch blogs', e);
+    console.error('Failed to fetch blogs', e);
+    blogError.value = e.message;
+  } finally {
+    blogLoading.value = false;
   }
 };
 
@@ -20,7 +26,13 @@ fetchBlogs();
 <template>
   <div class="container mx-auto min-w-xs">
     <h3 class="text-2xl">Blog</h3>
-    <div id="blog" class="m-1">
+    <div v-if="blogLoading" class="m-1">
+      <p>Loading...</p>
+    </div>
+    <div v-else-if="blogError" class="m-1">
+      <p>Failed to fetch blogs</p>
+    </div>
+    <div v-else id="blog" class="m-1">
       <BlogContent v-for="blog in blogs" :key="blog.id" :blog="blog" />
     </div>
   </div>
